effda11d60e0a54cb4e3ca356c25f322f8235a29
aria <aria.afsar@gmail.com>
2024-09-30 16:36:37 +0330

    added first version of user app models

--- quera/user/models.py
+++ quera/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Account(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 # Create your models here.
