a2b4cbde58eda0c2e63c6643a2d5c15b36d1127d
aria <aria.afsar@gmail.com>
2024-09-30 16:37:07 +0330

    registered account model in admin fiile

--- quera/user/admin.py
+++ quera/user/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin , register
+from .models import Account
+
+@register(Account)
+class AccountAdmin(ModelAdmin):
+    pass
 
 # Register your models here.
