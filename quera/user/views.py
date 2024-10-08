60c12061fd292bff952338a3c6de54bf2fbf3f81
aria <aria.afsar@gmail.com>
2024-10-08 22:48:18 +0330

    added login to user views

--- quera/user/views.py
+++ quera/user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Account
+from rest_framework_simplejwt.tokens import *
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+class Login():
+    pass
 
 # Create your views here.
