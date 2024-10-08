39b59751885799fbda6c27944dfff1c6b8d52c61
aria <aria.afsar@gmail.com>
2024-10-08 21:09:58 +0330

    registered models in admin

--- quera/class_app/admin.py
+++ quera/class_app/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+from django.contrib.admin import register , ModelAdmin
+from .models import Class
 
+@register(Class)
+class ClassAdmin(ModelAdmin):
+    pass
 # Register your models here.
