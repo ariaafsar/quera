421f92b872dbc86c34c399ba06797995baee0336
aria <aria.afsar@gmail.com>
2024-10-08 21:04:39 +0330

    added class models

--- quera/class_app/models.py
+++ quera/class_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from user.models import Account
+
+class Class(models.Model):
+    title = models.CharField(max_length=150)
+    creator = models.ForeignKey(Account , on_delete=models.PROTECT)
+    teachers = models.ManyToManyField(Account , related_name='teacher')
+    students = models.ManyToManyField(Account , related_name='student')
+    password = models.CharField(max_length=100 , min_length = 8)
 
 # Create your models here.
