6d41e9c2f2376afb9dbb31593888c9fd87cecabc
aria <aria.afsar@gmail.com>
2024-10-09 16:21:35 +0330

    edited login view in user views fiile

--- quera/user/views.py
+++ quera/user/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from .models import Account
-from rest_framework_simplejwt.tokens import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-class Login():
+class Login(TokenObtainPairView):
     pass
-
 # Create your views here.
