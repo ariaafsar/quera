c80f37155f0a7f2efdce20b130e6d3c4a50ab5d7
aria <aria.afsar@gmail.com>
2024-10-09 16:44:03 +0330

    added create class view in class views file

--- quera/class_app/views.py
+++ quera/class_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Class 
+from .serializers import CreateClassSerializer
+from rest_framework import generics , status
+from rest_framework.response import Response
 
+class CreteClass(generics.CreateAPIView):
+    serializer_class = CreateClassSerializer
+    queryset = Class.objects.all()
+    def CreateClass(self , request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
 # Create your views here.
