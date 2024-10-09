ebc261775bdfd32a60db6c8d9bec820659946d9f
aria <aria.afsar@gmail.com>
2024-10-09 17:30:49 +0330

    debuged user urls in user urls file

--- quera/user/urls.py
+++ quera/user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import Login , Refresh , Signup
 
-urlptterns = [
+urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('refresh/', Refresh.as_view(), name='refresh'),
     path('signup/', Signup.as_view(), name='signup'),
