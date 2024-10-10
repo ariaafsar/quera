6f1d23dacee09fa0d0bb399051a738c7fd203476
aria <aria.afsar@gmail.com>
2024-10-10 12:03:05 +0330

    edited user models

--- quera/user/models.py
+++ quera/user/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 
 class Account(models.Model):
     user = models.OneToOneField(User , on_delete=models.CASCADE)
-
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=100)
+    
     def __str__(self):
         return self.user.username
 
